986f107c78b6c605f259138ff75ab615c3a90c2d
ambarja <antony.barja8@gmail.com>
2022-08-10 22:08:18 -0500

    update

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const references = `
    <h5><u>References:</u></h5>
    <ul><li><p class="reference">Matesanz Nogales, A. (2009). Datos para la adaptación castellana de la Escala de Temores (FSS), Análisis y modificación de conducta, 35(152), 67-94.</p></li></ul>
    <ul><li><p class="reference">Pérez de la Dehesa, R. (1969). La editorial Sempere en Hispanoamérica y España. Revista Iberoamericana, XXXV(69), 551-555.</p></li></ul>
-   <ul style="padding-bottom: 10px;"><li><p class="reference">Strachota, S. (2020). Generalizaing in teh context of an early algebra intervention. Infancia y aprendizaje, 43(2), 347-394.</p></li></ul>
+   <ul style="padding-bottom: 10px; margin-top: 0px;"><li><p class="reference">Strachota, S. (2020). Generalizaing in teh context of an early algebra intervention. Infancia y aprendizaje, 43(2), 347-394.</p></li></ul>
   </div>
 `
 
@@ -87,7 +87,7 @@ function initMap() {
             container.append(chapter).append(imgHolder).append(source).append(description);
             $('#contents').append(container);
           }else{
-            var video = $('<video width="320" height="240" controls></video>', {
+            var video = $('<video width="320" height="200" controls></video>', {
             });
             var sourcevideo = $('<source>', {
               src: feature.properties['video'],
